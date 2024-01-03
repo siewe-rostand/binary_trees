e2c00ab345391edb889dd18294f23b3572aedd7b
siewe-rostand <rostand.siewe9@gmail.com>
2024-01-02 20:25:42 -0800

    task 3 - delete binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function to delete an entire binary tree
+ *
+ * @tree: root node of the tree to be deleted
+ *
+ * Return: nothing is return
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (NULL);
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
+}
