2380d09f98755ba1111cb00751a977c63b244927
siewe-rostand <rostand.siewe9@gmail.com>
2024-01-02 20:58:16 -0800

    task  6 - binary tree pre order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -15,6 +15,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 	func(tree->n);
 
-	binary_tree_preorder(tree->left);
-	binary_tree_preorder(tree->right);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
