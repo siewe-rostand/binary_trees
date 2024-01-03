d992d825d7177b29e52dc0461b474a486ddf31a4
siewe-rostand <rostand.siewe9@gmail.com>
2024-01-02 19:51:47 -0800

    task 0 finished

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,13 +11,13 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t newNode;
+	binary_tree_t *newNode;
 
 	newNode = malloc(sizeof(binary_tree_t));
-	if (newNode == null)
+	if (newNode == NULL)
 		return (NULL);
 	newNode->parent = parent;
-	newNode->item = value;
+	newNode->n = value;
 	newNode->left = NULL;
 	newNode->right = NULL;
 
