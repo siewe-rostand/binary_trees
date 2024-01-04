e20440a6f18bf28efa738c04189ef3fb3cfd1232
siewe-rostand <73501607+siewe-rostand@users.noreply.github.com>
2024-01-04 03:45:52 +0100

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,16 +10,16 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	if (tree)
+	{
+		size_t lt = 0, rt = 0;
 
-	size_t lt = 0, rt = 0;
+		lt = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		rt = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	lt = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	rt = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-	if (lt > rt)
-		return (lt);
-	else
-		return (rt);
+		if (lt > rt)
+			return (lt);
+		else
+			return (rt);
+	}
 }
