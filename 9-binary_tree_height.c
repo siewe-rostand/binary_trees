809e3359e125ee8f8fb9fa87ae0ac76aadd32046
siewe-rostand <rostand.siewe9@gmail.com>
2024-01-03 18:43:43 -0800

    task 9 - betty update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	size_t lt = 0, rt = 0;
-	
+
 	lt = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	rt = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
